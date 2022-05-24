53e7c0fbe09f6bd0ef9dd53540846f165910c5d8
Sneguradik <93195734+Sneguradik@users.noreply.github.com>
2022-05-24 21:29:25 +0300

    Add files via upload

--- Client/CameraHandler/CameraHandlerMain.py
+++ Client/CameraHandler/CameraHandlerMain.py
@@ -1,25 +1,23 @@
 import threading as th
-from unittest.loader import _SortComparisonMethod
-import cv2
+import cv2, socket, pickle, struct
+from math import sqrt
 from imutils.video import VideoStream
 import mediapipe as mp
 
 class DeamonCapturing():
     def run(self):
-        try:
-            IsItPiCamera = True
-            cap = VideoStream(src=0, usePiCamera=True, resolution=self.frameSize, framerate=32).start()
-        except:
-            IsItPiCamera = False
-            cap = cv2.VideoCapture(0)
+        
+        cap = cv2.VideoCapture('istockphoto-453649888-640_adpp_is.mp4')
 
         detector = PoseDetector()
         
         while True:
             _ , img = cap.read()
 
-            detector.findPose()
+            detector.findPose(img)
             lmlist = detector.getPosition(img)
+            detector.notice(lmlist, detector.ls, img)
+            #print(lmlist)
 
             cv2.imshow("Image", img)
             cv2.waitKey(1)
@@ -34,32 +32,69 @@ class PoseDetector():
 
         self.mpDraw = mp.solutions.drawing_utils
         self.mpPose = mp.solutions.pose
-        self.pose = self.mpPose.Pose(self.mode,self.upBody,self.smooth,self.detectionCon,self.trackCon)
+        self.pose = self.mpPose.Pose()
+        self.cnt = 0
+        self.ls = []
+
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.host_ip = ''
+        self.port = 9999
+        try:
+            self.client_socket.connect((self.host_ip, self.port))
+        except:
+            print('Ops!')
 
 
     def findPose(self, img  , draw=True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        self.results =self.pose.process(img(imgRGB))
+        self.results =self.pose.process(imgRGB)
          
         if self.results.pose_landmarks:
             if draw:
                 self.mpDraw.draw_landmarks(img, self.results.pose_landmarks, self.mpPose.POSE_CONNECTIONS)
 
+        self.mpDraw
         return img
 
     def getPosition(self, img, draw=True):
         lmlist = []
 
-        if self.results.pose_landmark:
+        if self.results.pose_landmarks:
             for id, lm in enumerate(self.results.pose_landmarks.landmark):
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
                 lmlist.append([id, cx, cy])
                 if draw:
                     cv2.circle(img, (cx, cy), 5, (255, 0 , 0), cv2.FILLED)
+            x=0
+            y=0
+            for el in lmlist:
+                x += el[1]
+                y += el[2]
+            coords = (int(x/len(lmlist)), int(y/len(lmlist)))
+            cv2.circle(img, coords, 5, (255, 234 , 0), cv2.FILLED)
+            print(coords)
+        return lmlist
+
+    def notice(self, lmlist, ls, img):
+        if self.cnt==0:
+            if lmlist == ls and lmlist:
+                #try:
+                    #a = pickle.dumps(img)
+                    #message = struct.pack("Q", len(a))+a
+                    #self.client_socket.sendall(message)
+                #except:
+                    #print('Error')
+                print(True)
+        if self.cnt<15:
+            self.cnt+=1
+        if self.cnt==15:
+            self.cnt = 0
+        
 
 def main():
-    x = DeamonCapturing.run()
-
+    x  = DeamonCapturing()
+    x.run()
+    
 if __name__ == "__main__":
     main()
